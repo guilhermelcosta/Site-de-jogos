0513707af9765bb3daaec1cd2a1d109bd4f1e920
Guilherme Costa <guilhermeldcosta@gmail.com>
2022-11-21 20:23:57 -0300

    Reformulação da área de plataformas

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Cards de recomendações de jogos.
 fetch('https://api.rawg.io/api/games?key=965cf8a1fa7f420387e453ca63050020')
   .then((resposta) => resposta.json())
   .then((data) => {
@@ -14,4 +15,24 @@ fetch('https://api.rawg.io/api/games?key=965cf8a1fa7f420387e453ca63050020')
               </div>`;
     }
     document.querySelector('.area_lancamento').innerHTML = str;
+  });
+
+// Cards de plataformas.
+fetch('https://api.rawg.io/api/platforms?key=965cf8a1fa7f420387e453ca63050020')
+  .then((resposta) => resposta.json())
+  .then((data) => {
+    let str = '';
+
+    for (let i = 0; i < 6; i++) {
+      let plataforma = data.results[i];
+
+      str += `<div class="card_plataforma">
+                <img src="${plataforma.image_background}" alt="">
+                 <div class="informacoes_plataforma">
+                   <h4><a href="#">${plataforma.name}</a></h4>
+                   <p><a href="#">Mais detalhes</a></p>
+                 </div>
+              </div>`;
+    }
+    document.querySelector('.area_plataforma').innerHTML = str;
   });
